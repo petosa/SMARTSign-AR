6ee7c76ea51a73f766425ca0c50950a3dbe3138a
Nick Peto <nick.petosa@gmail.com>
2016-05-26 20:05:06 -0400

    first

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+     
+.controller('signBookCtrl', function($scope) {
+
+})
+   
+.controller('cameraCtrl', function($scope) {
+
+})
+   
+.controller('oCRTextCtrl', function($scope) {
+
+})
+   
+.controller('oCRText2Ctrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,63 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.signBook', {
+    url: '/signbook',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/signBook.html',
+        controller: 'signBookCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.camera', {
+    url: '/camera',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/camera.html',
+        controller: 'cameraCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.oCRText', {
+    url: '/entry',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/oCRText.html',
+        controller: 'oCRTextCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.oCRText2', {
+    url: '/ocrtext',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/oCRText2.html',
+        controller: 'oCRText2Ctrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/signbook')
+
+  
+
+});
